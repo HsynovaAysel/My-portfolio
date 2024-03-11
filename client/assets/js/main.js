c7ca9c57dc42ced90336cbfc83fd8e96b61a2503
gd7uz3mha@code.edu.az <gd7uz3mha@code.edu.az>
2024-03-11 11:45:59 -0700

    committed

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -6,10 +6,49 @@ listAll.forEach((item) =>
   })
 );
 
-let gear=document.querySelector('.gear')
-let templateBox=document.querySelector('.template-box')
+let gear = document.querySelector(".gear");
+let templateBox = document.querySelector(".template-box");
 
-gear.addEventListener('click',function(){
-    // console.log('salam');
-templateBox.classList.toggle('template-box-block')
-})
+gear.addEventListener("click", function () {
+  templateBox.classList.toggle("template-box-block");
+});
+let aboutPage = document.querySelector("#about");
+let aboutli = document.querySelector(".about-li");
+aboutli.addEventListener("click", function () {
+  aboutPage.classList.add("about-page");
+  resumePage.classList.remove("resume-page");
+  portfolioPage.classList.remove("portfolio-page");
+  contactPage.classList.remove("contact-page");
+});
+let homePage = document.querySelector("#home");
+let homeli = document.querySelector(".home-li");
+homeli.addEventListener("click", function () {
+  aboutPage.classList.remove("about-page");
+  resumePage.classList.remove("resume-page");
+  portfolioPage.classList.remove("portfolio-page");
+  contactPage.classList.remove("contact-page");
+});
+let resumePage = document.querySelector("#resume");
+let resumeli = document.querySelector(".resume-li");
+resumeli.addEventListener("click", function () {
+  aboutPage.classList.remove("about-page");
+  resumePage.classList.add("resume-page");
+  portfolioPage.classList.remove("portfolio-page");
+  contactPage.classList.remove("contact-page");
+});
+let portfolioPage = document.querySelector("#portfolio");
+let portfolioli = document.querySelector(".portfolio-li");
+portfolioli.addEventListener("click", function () {
+  aboutPage.classList.remove("about-page");
+  resumePage.classList.remove("resume-page");
+  portfolioPage.classList.add("portfolio-page");
+  contactPage.classList.remove("contact-page");
+});
+let contactPage = document.querySelector("#contact");
+let contactli = document.querySelector(".contact-li");
+contactli.addEventListener("click", function () {
+  aboutPage.classList.remove("about-page");
+  resumePage.classList.remove("resume-page");
+  portfolioPage.classList.remove("portfolio-page");
+  contactPage.classList.add("contact-page");
+});
